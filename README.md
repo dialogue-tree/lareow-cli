1bdf0efedc9092be5e262ce7611751027e573074
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2019-03-09 22:55:43 +0000

    renamed project and moved to yargs from commander

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Static-gen
+# Lareow-cli
 
-Dialogue tree static site generator CLI.
+Lareow static site generator CLI.
 
 ## Usage
 
-Install globally with `npm install -g @dialoguetree/static-gen` or run with `npx` like in examples below.
+Install globally with `npm install -g @dialoguetree/lareow` or run with `npx` like in examples below.
 
 ### New site
-Create new site with `npx @dialoguetree/static-gen init` (or `static-gen init` if installed globally). This will create a folder structure and a site.config.json to get starts. Careful, this can and will overwrite files if they already exist.
+Create new site with `npx @dialoguetree/lareow init` (or `lareow init` if installed globally). This will create a folder structure and a site.config.json to get starts. Careful, this can and will overwrite files if they already exist.
 
 ### Building
-Build your site with `npx @dialoguetree/static-gen build <site.config.json>`. You only need to provide the file path if it is different from the default.
+Build your site with `npx @dialoguetree/lareow build [site.config.json]`. You only need to provide the file if it is different from the default.
 
 ## Templates
 Currently templating is supported via EJS (https://ejs.co/).
--- bin/cli.js
+++ bin/cli.js
@@ -1,55 +1,49 @@
 #!/usr/bin/env node
-const debug = require('debug')('SiteGen:CLI');
-const commander = require('commander');
+const debug = require('debug')('Lareow:cli');
+const yargs = require('yargs');
 const colors = require('colors/safe');
 const fs = require('fs-extra');
-const {Builder, Initializer} = require('../lib');
-const version = require('../package.json').version;
+const { Builder, Initializer } = require('../lib');
 
-commander
-	.name('static-gen')
-	.version(version, '-v, --version');
-
-commander
-	.command('init')
-	.description('Initialize a new site, creating files and a folder structure. Warning, can overwrite some files.')
-	.action( async () => {
-		debug('Initializing with default config');
-		console.log(colors.green('Initializing new site.'));
-		Initializer.init();
-	});
-
-commander
-	.command('build [configjson]')
-	.description('Build site based on provided config, or looks for `site.config.json` by default')
-	.action( async (configjson) => {
-		if (!configjson) {
-			configjson = './site.config.json';
+yargs
+	.scriptName('Lareow-cli')
+	.demandCommand(1)
+	.usage('$0 <cmd> [args]')
+	// .command('version', 'Returns the version of the tool')
+	.command(
+		'init',
+		'Initialize a new site in the current directory',
+		{},
+		async () => {
+			debug('Initializing with default config');
+			console.log(colors.green('Initializing new site.'));
+			Initializer.init();
 		}
+	)
+	.command(
+		'build [config]',
+		'Build site from templates into the output directory',
+		yargs => {
+			yargs.positional('config', {
+				describe: 'Config file describing the site',
+				default: './site.config.json'
+			});
+		},
+		async argv => {
+			try {
+				if (await fs.exists(argv.config)) {
+					const siteConfig = await fs.readJSON(argv.config);
+					debug('loaded config', argv.config);
 
-		try {
-			if (await fs.exists(configjson)) {
-				const siteConfig = await fs.readJSON(configjson);
-				debug('loaded config', configjson);
-
-				const builder = new Builder(siteConfig);
-				builder.build();
-			} else {
-				console.log(colors.red(`File ${configjson} does not exist...`));
+					const builder = new Builder(siteConfig);
+					builder.build();
+				} else {
+					console.log(colors.red(`File ${argv.config} does not exist, cannot continue`));
+				}
+			} catch (error) {
+				console.error(error);
 			}
-		} catch (error) {
-			console.error(error);
 		}
-	});
-
-commander
-	.command('*', 'foo', {})
-	.action( async () => {
-		commander.outputHelp();
-	});
-
-commander.parse(process.argv);
-
-if (process.argv.length == 2) {
-	commander.outputHelp();
-}
+	)
+	.help()
+	.argv;
